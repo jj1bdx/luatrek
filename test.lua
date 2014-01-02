ba95c4f3a130a3f96bd6ec709e17bbd6e465f30c
Kenji Rikitake <kenji.rikitake@acm.org>
2014-01-02 22:14:35 +0900

    Update test.lua
    * Add string.ends() function
      see <http://lua-users.org/wiki/StringRecipes>

--- test.lua
+++ test.lua
@@ -1,5 +1,16 @@
 #!/usr/bin/env lua
+
+function string.ends(String,End)
+    return End=='' or string.sub(String,-string.len(End))==End
+end
+
+test1="comp 10 20;"
+test2="comp 10 20"
+print(test1, string.ends(test1, ";"))
+print(test2, string.ends(test2, ";"))
+
 io.write("Hello? ")
 io.flush()
 x, y = io.read("*n", "*n")
 io.write("x= ", x, " y= ", y, "\n")
+
