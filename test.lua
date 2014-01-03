386ad530c7a659b93e2f5e2efd2ede9f4b981cfd
Kenji Rikitake <kenji.rikitake@acm.org>
2014-01-03 22:18:03 +0900

    Update test.lua
    * io.read("*l") test

--- test.lua
+++ test.lua
@@ -11,6 +11,7 @@ print(test2, string.ends(test2, ";"))
 
 io.write("Hello? ")
 io.flush()
-x, y = io.read("*n", "*n")
-io.write("x= ", x, " y= ", y, "\n")
-
+-- x, y = io.read("*n", "*n")
+-- io.write("x= ", x, " y= ", y, "\n")
+s = io.read("*l")
+io.write("s= ", s)
