490ef3a0f22b4fb2667a020a17cc8e51bf4e9761
Kenji Rikitake <kenji.rikitake@acm.org>
2014-01-04 22:07:24 +0900

    Add template.lua
    * Module template

---
+++ template.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/env lua
+--- Module template: summary here
+-- @module template
+-- @alias M
+
+local M = {}
+
+-- first function
+function M.one()
+-- ..
+end
+
+-- End of module
+return M
+
+-- vim: set ts=4 sw=4 sts=4 et :
+-- emacs: -*- mode:lua; tab-width:4; indent-tabs-mode:nil;  -*-
