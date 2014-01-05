ae0febd5f7cf08e9ce42bf01ff1b9dc547695a07
Kenji Rikitake <kenji.rikitake@acm.org>
2014-01-05 21:33:51 +0900

    Update test.lua
    * Add utils.create2darray() test

--- test.lua
+++ test.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env lua
 
 getpar = require "getpar"
+utils = require "utils"
 
 function string.ends(String,End)
     return End=='' or string.sub(String,-string.len(End))==End
@@ -13,9 +14,19 @@ print(test2, string.ends(test2, ";"))
 
 n, t = getpar.getwords("test_prompt")
 print("number of words=", n)
-local j = 0
 for i, v in ipairs(t) do
     print("t[", i, "]=", v)
 end
 
 print(getpar.getynpar("Yes or no please"))
+
+local a = utils.create2darray(4, 5, "elem")
+a[1][1] = 1
+a[2][2] = 2
+a[3][3] = 3
+a[4][4] = 4
+for i, t1 in ipairs(a) do
+    for j, v in ipairs(t1) do
+        print("a[", i, "][", j, "]=", v)
+    end
+end
