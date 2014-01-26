bc877e07dc2bd40705779d82973efd1409bcf3c2
Kenji Rikitake <kenji.rikitake@acm.org>
2014-01-26 20:06:58 +0900

    Update test.lua
    * Make variables local
    * Add test for trek.getpar.getcodpar()

--- test.lua
+++ test.lua
@@ -7,12 +7,12 @@ function string.ends(String,End)
     return End=='' or string.sub(String,-string.len(End))==End
 end
 
-test1="comp 10 20;"
-test2="comp 10 20"
+local test1="comp 10 20;"
+local test2="comp 10 20"
 print(test1, string.ends(test1, ";"))
 print(test2, string.ends(test2, ";"))
 
-n, t = trek.getpar.getwords("test_prompt")
+local n, t = trek.getpar.getwords("test_prompt")
 print("number of words=", n)
 for i, v in ipairs(t) do
     print("t[", i, "]=", v)
@@ -24,3 +24,10 @@ print(V.NSECTS)
 print(V.Quad[1][1].bases)
 V.Quad[1][1].bases = 1
 print(V.Quad[1][1].bases)
+
+local val, n, t = trek.getpar.getcodpar("Game Length", V.Lentab)
+print(val)
+print("number of words=", n)
+for i, v in ipairs(t) do
+    print("t[", i, "]=", v)
+end
