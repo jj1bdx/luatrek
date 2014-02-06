76ae5827158e284e590ddace3403a2596553653c
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-06 20:44:48 +0900

    Update test.lua
    * Comment out Quad content dumping routine

--- test.lua
+++ test.lua
@@ -4,6 +4,7 @@ trek = require "trek"
 local V = trek.gstate
 trek.setup.setup()
 
+--[[
 for i = 1, V.NQUADS do
     for j = 1, V.NQUADS do
         local q = V.Quad[i][j]
@@ -11,3 +12,4 @@ for i = 1, V.NQUADS do
             q.holes, q.stars, q.systemname)
     end
 end
+]]
