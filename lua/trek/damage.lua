e699b11517030c35721ce5465a348b6658880e1e
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-07 17:11:08 +0900

    Update damage.lua
    * Add trek.damage.damaged() as in BSDtrek damaged.c

---
+++ lua/trek/damage.lua
@@ -0,0 +1,120 @@
+#!/usr/bin/env lua
+--- Luatrek ship damage control and report
+-- @module trek.damage
+-- @alias M
+
+-- Luatrek license statement
+--[[
+Luatrek ("this software") is covered under the BSD 3-clause license.
+
+This product includes software developed by the University of California, Berkeley
+and its contributors.
+
+Copyright (c) 2013, 2014 Kenji Rikitake. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+* Neither the name of Kenji Rikitake, k2r.org, nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This software incorporates portions of the BSD Star Trek source code,
+distributed under the following license:
+
+Copyright (c) 1980, 1993
+     The Regents of the University of California.  All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. All advertising materials mentioning features or use of this software
+   must display the following acknowledgement:
+     This product includes software developed by the University of
+     California, Berkeley and its contributors.
+4. Neither the name of the University nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+[End of LICENSE]
+]]
+
+--- Make module strict by Penlight pl.strict.module()
+local strict = require "pl.strict"
+local M = strict.module()
+--- Shorthand for trek.gstate prefix
+local V = require "trek.gstate"
+--- Global Game table
+local Game = V.Game
+--- Global Param table
+local Param = V.Param
+--- Global Ship table
+local Ship = V.Ship
+--- Global Now table
+local Now = V.Now
+--- Global Event table
+local Event = V.Event
+--- Global Quad table
+local Quad = V.Quad
+--- Global Sect table
+local Sect = V.Sect
+--- Global Move table
+local Move = V.Sect
+
+--- Check for damaged devices
+-- This is a boolean function which returns true if the
+-- specified device is broken.  It does this by checking the
+-- event list for a "device fix" action on that device.
+-- @string dev Device identifier string
+-- @treturn bool true if broken, false if not
+function M.damaged (dev)
+    for i = 1, M.MAXEVENTS do
+        local e = Event[i]
+        if (e.evcode == "E_FIXDV") and (e.systemname == d) then
+            return true
+        end
+    end
+	-- device fix not in event list -- device must not be broken
+    return false
+end
+
+-- End of module
+return M
+
+-- vim: set ts=4 sw=4 sts=4 et :
+-- emacs: -*- mode:lua; tab-width:4; indent-tabs-mode:nil;  -*-
