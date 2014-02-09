5fa6e5ebf2e889853950d056a7cf903cdae32105
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-09 14:45:11 +0900

    test.lua
    * Add short range scanner and klingon info dump

--- test.lua
+++ test.lua
@@ -3,6 +3,15 @@
 trek = require "trek"
 local V = trek.gstate
 trek.setup.setup()
+trek.srscan.srscan(-1)
+
+for i = 1, V.Etc.nkling do
+    local k = V.Etc.klingon[i]
+    pl.utils.printf("Etc.klingon[%d]: x = %d, y = %d, power = %d, dist = %f\n",
+    -- avgdist = %d, srndreq = %s\n",
+                     i, k.x, k.y, k.power, k.dist)
+                     --, k.avgdist, k.srndreq)
+end
 
 --[[
 for i = 1, V.NQUADS do
