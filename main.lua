c3f2f4c76cc0f2fa8d87b9f6c9a1f898f6064182
Kenji Rikitake <kenji.rikitake@acm.org>
2014-02-17 18:35:30 +0900

    Add main.lua
    * Example main program for Luatrek

---
+++ main.lua
@@ -0,0 +1,8 @@
+#!/usr/bin/env lua
+
+--- This is an example main program for luatrek
+
+--- load trek as a global variable here
+-- otherwise the program will not work!
+trek = require "trek"
+trek.play.main()
